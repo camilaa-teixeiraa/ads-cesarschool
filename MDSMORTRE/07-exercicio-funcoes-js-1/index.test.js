25683b4645bdf717243d44fa13cffd162ef60f7f
Camila Maria Teixeira <85718681+camilaa-teixeiraa@users.noreply.github.com>
2024-10-23 19:56:40 -0300

    Delete MDSMORTRE directory

--- MDSMORTRE/07-exercicio-funcoes-js-1/index.test.js
+++
@@ -1,8 +0,0 @@
-//Não faça nada neste arquivo, esqueça que ele existe
-const { execSync } = require('child_process');
-
-test('verifica a saída da função saudar', () => {
-  const output = execSync('node index.js').toString();
-  const expectedOutput = "Olá Mundo!\n"; 
-  expect(output).toBe(expectedOutput);
-});
